f7ec2821fa18b90dfcfb14b05c697ee6f9710278
Felix Schlegel <felix.schlegel@alumni.ethz.ch>
2022-12-11 10:40:09 +0100

    first component LearningResource.vue with styling

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-    <ul></ul>
+    <ul>
+        <LearningResource v-for="resource in storedResources" :key="resource.id" :title="resource.title"
+            :description="resource.description" :link="resource.link" />
+    </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue'
 export default {
+    components: {
+        LearningResource
+    },
     data() {
         return {
             storedResources: [
@@ -25,3 +32,19 @@ export default {
     }
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
