8cb451c2ae00a5235a9c3b68d5314498035485ac
Felix Schlegel <felix.schlegel@alumni.ethz.ch>
2022-12-11 18:07:20 +0100

    basebutton.vue

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Vue.js demo app
 
-Based on udemy course "Vue - The Complete Guide" section 10.
+Based on udemy course ["Vue - The Complete Guide"](https://www.udemy.com/course/vuejs-2-the-complete-guide) section 10.
+
+## vue concepts applied:
+
+- **Slots**: In BaseCard.vue
+- **Dynamic styling**: In BaseButton.vue. Shows how to have one global component for all buttons that can then be used in different variations.
---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,42 @@
+<template>
+    <button :type="type" :class="mode">
+        <slot></slot>
+        <!-- Alternatively, could accept another prop and use {{buttontext}}, but slot gives more flexibility, e.e. using svg icon -->
+    </button>
+</template>
+<!-- To give the button an alternative look, apply the .flat css class defined here under style with:
+<base-button mode="flat"></base-button> -->
+
+<script>
+export default {
+    props: ['type', 'mode']
+}
+</script>
+
+<style scoped>
+button {
+    padding: 0.75rem 1.5rem;
+    font-family: inherit;
+    background-color: #3a0061;
+    border: 1px solid #3a0061;
+    color: white;
+    cursor: pointer;
+}
+
+button:hover,
+button:active {
+    background-color: #270041;
+    border-color: #270041;
+}
+
+.flat {
+    background-color: transparent;
+    color: #3a0061;
+    border: none;
+}
+
+.flat:hover,
+.flat:active {
+    background-color: #edd2ff;
+}
+</style>
